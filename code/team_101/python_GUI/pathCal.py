472eb05a0ceabf3f2e30425dbca5a6e4ec51a3e3
insunday <57553609+insunday@users.noreply.github.com>
2020-05-23 13:02:47 +0800

    edit terminal command for navigation

--- code/team_101/python_GUI/pathCal.py
+++ code/team_101/python_GUI/pathCal.py
@@ -1,7 +1,17 @@
 import os
+import subprocess
+import time
+
 class pathCal:
     def __init__(self):
         self.path = ''
 
     def move(self): #调用ROS包选取导航轨迹
-        os.system("gnome-terminal -e 'bash -c \"roslaunch wpb_home_tutorials nav.launch; exec bash\"'")
+        subprocess.Popen(['gnome-terminal', '--disable-factory', '-e',
+                          'bash  -c \"roslaunch team_101 robot_spawn.launch; exec bash\"'],
+                         preexec_fn=os.setpgrp)
+        time.sleep(10)
+        subprocess.Popen(['gnome-terminal', '--disable-factory', '-e',
+                          'bash -c \"roslaunch team_101 navigation.launch; exec bash\"'],
+                         preexec_fn=os.setpgrp)
+
