af7aae4489799a51e4cb5d4df39005f905aa4947
PanLinyu <panlinyuppp@outlook.com>
2020-06-07 10:32:19 +0800

    beautify exception GUI

--- code/team_101/python_GUI/ExceptionHandler.py
+++ code/team_101/python_GUI/ExceptionHandler.py
@@ -4,11 +4,18 @@ import time
 import os
 from tkinter import *
 import sys
+from PIL import *
+
 class Exception:
     def __init__(self,type,current,root):
         self.ExceptionType = type #异常类型
         self.current = current #记录当前行为
         self.root = root #
+    
+    def get_image(filename, width, height):
+    im = PIL.Image.open(filename).resize((width,height))
+    return ImageTk.PhotoImage(im)
+
     '''
     Different types of defects:
     0 : 停止机器人[使用场景: 撞到物体时等]
@@ -29,14 +36,11 @@ class Exception:
         elif self.ExceptionType == 2 :
             print("action to be complete in grab")
         elif self.ExceptionType == 3 :
-            root = Tk()
-            root.title('错误')
-            root.geometry('500x150')
-            lb = Label(root, text='检测到长时间未建立地图，若无法建图,请重启程序', \
-                       fg='black', \
-                       font=(24), \
-                       width=50, \
-                       height=2, \
-                       relief=SUNKEN)
-            lb.pack()
+            root.title('超时错误')
+            root.geometry('450x200')
+            root.resizable(False, False)
+            canvas_root = tkinter.Canvas(root, width=450, height=200)
+            im_root = get_image('Timeout_error.png', 450, 200)
+            canvas_root.create_image(220,100, image=im_root)
+            canvas_root.pack()
             root.mainloop()
